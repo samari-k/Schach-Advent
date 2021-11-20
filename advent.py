34a77a2299ca14dd2791320b61699588be3bdf37
samari-k <safi.kuehn@gmx.de>
2021-11-20 21:37:45 +0100

    improve game

--- advent.py
+++ advent.py
@@ -4,7 +4,8 @@ from datetime import date
 
 
 """
-Matt in einem Zug, Aufgaben von https://www.schach-tipps.de/schachtraining/taktik/matt-in-1-zug
+Aufgaben von https://www.schach-tipps.de/schachtraining/taktik/matt-in-1-zug
+Verkürzte algebraische Notation: https://de.wikipedia.org/wiki/Schachnotation#Verk%C3%BCrzte_algebraische_Notation
 """
 
 
@@ -15,7 +16,7 @@ class AdventCalendar:
 
         self.focus_border_color = py_cui.CYAN_ON_BLACK
 
-        self.current_date = date.today().strftime("%d") if date.today() >= date(2021, 11, 1) else 0
+        self.current_date = date.today().strftime("%d") if date.today() >= date(2021, 11, 1) else "00"
         self.closed = ['0'+str(i) for i in range(1, 10) if i > int(self.current_date)] + \
                       [str(i) for i in range(10, 25) if i > int(self.current_date)]
         self.opened = ['0'+str(i) for i in range(1, 10) if '0'+str(i) not in self.closed] + \
@@ -27,7 +28,7 @@ class AdventCalendar:
 
         self.solution = ""
 
-        self.puzzles_scroll_menu = self.master.add_scroll_menu('Türchen', 0, 0, column_span=1, row_span=6)
+        self.puzzles_scroll_menu = self.master.add_scroll_menu('Rätsel', 0, 0, column_span=1, row_span=6)
         self.puzzles_scroll_menu.add_key_command(py_cui.keys.KEY_ENTER, self.select_puzzle)
         self.puzzles_scroll_menu.set_focus_border_color(self.focus_border_color)
         with open("puzzles.json", "r") as puzzles_file:
@@ -40,7 +41,8 @@ class AdventCalendar:
 
         self.puzzle_text_block = self.master.add_text_block('Advent, Advent!', 0, 1, column_span=3, row_span=5)
         self.puzzle_text_block.set_selectable(False)
-        self.puzzle_text_block.add_text_color_rule(r'(\|.*)|(\+.*)|(a\s+b.*)', py_cui.BLACK_ON_WHITE, 'contains')
+        self.puzzle_text_block.add_text_color_rule(r'(\|.*)|(\+.*)|(a\s+b.*)|(h\s+g.*)', py_cui.BLACK_ON_WHITE,
+                                                   'contains', match_type='region', region=[1, 37])
 
         self.answer_text_box = self.master.add_text_box('Antwort', 5, 1, column_span=3)
         self.answer_text_box.add_key_command(py_cui.keys.KEY_ENTER, self.check_solution)
@@ -59,20 +61,35 @@ class AdventCalendar:
         with open("puzzles.json", "w") as puzzles_file:
             json.dump(self.puzzles, puzzles_file)
 
-    def board_to_string(self, board):
-        board_string = "     a   b   c   d   e   f   g   h \n" \
-                       "   +-------------------------------+\n"
-        i = 8
+    def board_to_string(self, board, flip: bool = False):
+        # flip Board if it's black's turn
+
+        border = "   +-------------------------------+\n"
+
+        if not flip:
+            letter_row = "     a   b   c   d   e   f   g   h \n"
+            board_string = letter_row + border
+            i = 8
+        else:
+            letter_row = "     h   g   f   e   d   c   b   a \n"
+            board_string = border
+            i = 1
+
         for row in board:
             board_string += f"  {int(i)}|"
             for field in row:
                 board_string += self.pieces[field] + '|'
             board_string += "\n"
-            if i > 1:
+
+            if (i > 1 and not flip) or (i < 8 and flip):
                 board_string += "   |---+---+---+---+---+---+---+---|\n"
-            if i == 1:
-                board_string += "   +-------------------------------+\n\n"
-            i -= 1
+
+            i = i-1 if not flip else i+1
+
+        if not flip:
+            board_string += border
+        else:
+            board_string += (border + letter_row)
         return board_string
 
     def load_scroll_menu(self):
@@ -81,12 +98,14 @@ class AdventCalendar:
             self.puzzles_scroll_menu.add_item(puzzle)
 
     def select_puzzle(self):
+        self.answer_text_box._text_color_rules.clear()
         puzzle_name = self.puzzles_scroll_menu.get()
         if puzzle_name[-2:] not in self.closed:
             self.puzzle_text_block.clear()
             puzzle = self.puzzles.get(puzzle_name)
-            self.puzzle_text_block.set_text(self.board_to_string(puzzle.get("board")) +
-                                            puzzle.get("description"))
+            flip = True if puzzle["turn"] == "schwarz" else False
+            self.puzzle_text_block.set_text(self.board_to_string(puzzle.get("board"), flip) +
+                                            "\n " + puzzle.get("turn").capitalize() + " am Zug.")
             self.puzzle_text_block.set_title(puzzle_name)
             self.solution = puzzle.get("solution")
             if puzzle["solved"]:
@@ -110,7 +129,7 @@ class AdventCalendar:
     def check_solution(self):
         answer = self.answer_text_box.get()
         if answer == self.solution:
-            self.master.show_message_popup(title="Super!", text="Das war richtig.", color=py_cui.BLACK_ON_GREEN)
+            self.master.show_message_popup(title="Super!", text="Das war richtig.", color=py_cui.WHITE_ON_GREEN)
             self.color_solved()
             puzzle_name = self.puzzles_scroll_menu.get()
             puzzle = self.puzzles.get(puzzle_name)
@@ -124,7 +143,6 @@ class AdventCalendar:
             self.master.move_focus(self.answer_text_box)
 
     def rename_puzzle(self, puzzle_name):
-        puzzle = self.puzzles.get(puzzle_name)
         if puzzle_name[-1] == "*":
             new_name = puzzle_name[:-1]
         else:
@@ -162,26 +180,24 @@ class AdventCalendar:
 
     def show_manual(self):
         text = "\n" \
-               "Darum geht es:\n" \
-               "--------------\n" \
-               "  Hier findest du eine Sammlung an \"Matt in \n" \
-               "  einem Zug\"-Rätsel. Jeden Tag im Dezember \n" \
-               "  kommt eines hinzu.\n\n" \
                "Navigation:\n" \
                "-----------\n" \
                "  Benutze die Pfeiltasten um zwischen den Feldern\n" \
-               "  und den Türchen zu navigieren. Enter wählt ein \n" \
-               "  Feld, Button oder Türchen aus. Mit Escape lässt \n" \
-               "  sich die Auswahl eines Feldes beenden. Ein \n" \
-               "  ausgewähltes Feld erkennst du an der farbigen \n" \
-               "  Umrandung\n\n" \
+               "  und den Rätseln zu navigieren. Enter wählt ein \n" \
+               "  Feld, Button oder Rätsel aus. Mit Escape lässt \n" \
+               "  sich die Auswahl eines Feldes beenden.\n\n" \
                "Notation:\n" \
                "---------\n" \
                "  Die Antwort wird in der verkürzten algebraischen\n" \
                "  Notation angegeben, wobei hier auch der Bauer ein \n" \
-               "  voran gestelltes 'B' erwartet. \n" \
-               "  Beispiel: Dg6 wenn die Dame auf das Feld g6 \n" \
-               "  ziehen muss für das Matt."
+               "  voran gestelltes 'B' erwartet. \n\n" \
+               "  Beispiel: Dg6# wenn die Dame auf das Feld g6 zieht.\n" \
+               "  und den gegnerischen König Matt setzt.\n" \
+               "  Wenn bei dem Zug eine gegnerische Figur geschlagen\n" \
+               "  wird, kommt zwischen Figur und Zielfeld ein x: \n" \
+               "  Sxc4# bedeutet also: Springer zieht nach c4, schlägt \n" \
+               "  dort eine gegnerische Figur und setzt den König matt.\n\n" \
+               "  Mehr Infos: de.wikipedia.org/wiki/Schachnotation"
         self.puzzle_text_block.set_text(text)
         self.puzzle_text_block.set_title("Spielanleitung")
         self.disable_hint()
